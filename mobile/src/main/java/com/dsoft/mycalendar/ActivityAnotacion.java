fa12773be361ac11e0261e0e22e738ab05caaca0
marcobarrios <mabarriosmarco@gmail.com>
2014-10-15 13:17:03 -0600

    Se añadio interfaz para nueva anotacion

---
+++ mobile/src/main/java/com/dsoft/mycalendar/ActivityAnotacion.java
@@ -0,0 +1,15 @@
+package com.dsoft.mycalendar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Marco Barrios on 15/10/2014.
+ */
+public class ActivityAnotacion extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_crear_nota);
+    }
+}
--- mobile/src/main/java/com/dsoft/mycalendar/FragmentAnotaciones.java
+++ mobile/src/main/java/com/dsoft/mycalendar/FragmentAnotaciones.java
@@ -1,10 +1,13 @@
 package com.dsoft.mycalendar;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
 import com.faizmalkani.floatingactionbutton.FloatingActionButton;
 /**
  * Created by Marco Barrios on 08/10/2014.
@@ -12,6 +15,7 @@ import com.faizmalkani.floatingactionbutton.FloatingActionButton;
 public class FragmentAnotaciones extends Fragment {
     View v;
     FloatingActionButton btn;
+    protected static final int REQUEST_CODE = 10;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -26,8 +30,23 @@ public class FragmentAnotaciones extends Fragment {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent i =  new Intent(getActivity().getApplicationContext(), ActivityAnotacion.class);
+                //i.putExtra("fecha", String.valueOf(calendario.getDate()));
+                startActivity(i);
             }
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        try {
+            super.onActivityResult(requestCode, resultCode, data);
+            if (requestCode == REQUEST_CODE) {
+                String result = data.getStringExtra("result");
+                Toast.makeText(getActivity().getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+            }
+        }catch(Exception exp) {
+
+        }
     }
 }
